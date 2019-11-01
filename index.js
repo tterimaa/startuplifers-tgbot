4dd011339a32201acf177db83222ba3b35ed9c63
tterimaa <tterimaa@gmail.com>
2019-11-01 09:56:45 +0200

    Save jobs in file. Check api for changes working in local.

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 require('dotenv').config()
-const db = require('./db')
 const cron = require('node-cron')
 const jobService = require('./services/jobs')
-const Bot = require('node-telegram-bot-api')
-
-const token = process.env.TOKEN
 const url = 'http://localhost:3001/jobs'
-const bot = new Bot(token)
+const bot = require('./bot')
 
-jobService.getAll(url).then(data => {
-    db.defaults({ jobs: [data]}).write()
-    console.log('db defaults set')
-})
+jobService.initJobs()
 
 console.log(`Bot server started in the ${process.env.NODE_ENV} mode`)
 
-cron.schedule('* * * * *', async () => {
-    const inDb = db.getState()
-    console.log(inDb.jobs)
+const compareId = (a,b) => a.id > b.id
+
+const arraysEqual = (a, b) => {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    a.sort(compareId)
+    b.sort(compareId)
+
+    for (let i = 0; i < a.length; ++i) {
+      if (JSON.stringify(a[i]) !== JSON.stringify(b[i])) {
+        return false;
+      }
+    }
+    return true;
+}
+
+const cronJob = async () => {
+    const jobsLocal = jobService.readJobs()
+    console.log(jobsLocal)
     const inApi = await jobService.getAll(url)
     console.log(inApi)
 
-    // bot.sendMessage('@testchannel_randomfacts', res.data.fact)
-})
+    if(!arraysEqual(jobsLocal, inApi)) {
+      console.log('JOB LISTING HAS CHANGED IN API (or local is empty)')
+      jobService.writeJobs(inApi)
+    }
+}
+
+process.env.NODE_ENV === 'development' ?
+  cronJob() :
+  cron.schedule('* * * * *', cronJob)
--- services/jobs.js
+++ services/jobs.js
@@ -1,10 +1,34 @@
 const axios = require('axios')
+const fs = require('fs')
 
-const url = 'http://localhost:3001/jobs'
+const testapiurl = 'http://localhost:3001/jobs'
 
 const getAll = async () => {
-    const res = await axios.get(url)
+    const res = await axios.get(testapiurl)
     return res.data 
 }
 
-module.exports = { getAll }
+const initJobs = () => {
+    let initialJobs
+    if(fs.existsSync('jobs.json')) {
+        const rawdata = fs.readFileSync('jobs.json') // initialization in test from file
+        initialJobs = JSON.parse(rawdata)
+    }
+    else initialJobs = { jobs: [] } // In production init with initial api data
+    fs.writeFileSync('jobs.json', JSON.stringify(initialJobs))
+}
+
+const readJobs = () => {
+    const rawdata = fs.readFileSync('jobs.json')
+    const json = JSON.parse(rawdata)
+    return json.jobs
+}
+
+const writeJobs = (newJobs) => {
+    const jobs = {
+        jobs: newJobs
+    }
+    fs.writeFileSync('jobs.json', JSON.stringify(jobs))
+}
+
+module.exports = { getAll, initJobs, readJobs, writeJobs }
